0fe07c05915b303978743e81aa372267e62ba6bf
henriquebuzon <henriquebzn1@gmail.com>
2023-03-25 10:44:17 -0300

    Seeding the database

---
+++ backend/seed/import.ts
@@ -0,0 +1,50 @@
+import mockData from "./mock-data";
+
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const importMockData = async () => {
+  for (const movie of mockData) {
+    await prisma.movie.create({
+      data: {
+        name: movie.name,
+        description: movie.description,
+        genre: movie.genre,
+      },
+    });
+  }
+  console.log("Data successfully imported!");
+};
+
+const deleteData = async () => {
+  try {
+    await prisma.movie.deleteMany();
+    console.log("Data successfully deleted!");
+  } catch (err) {
+    console.log(err);
+  }
+  await prisma.$disconnect();
+};
+
+if (process.argv[2] === "--import") {
+  importMockData()
+    .catch((e) => {
+      console.error(e);
+      process.exit(1);
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+}
+
+if (process.argv[2] === "--delete") {
+  deleteData()
+    .catch((e) => {
+      console.error(e);
+      process.exit(1);
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+}
---
+++ backend/seed/mock-data.ts
@@ -0,0 +1,104 @@
+export default [
+  {
+    name: "The Destroyer of Worlds 3",
+    description:
+      "He's back. He is strong. He is evil. And he only cares about one thing: destroying the whole world. Prepare yourself for hardcore destruction.",
+    genre: "Science Fiction",
+  },
+  {
+    name: "Attack of the Killer Tomatoes",
+    description:
+      "A group of scientists band together to save the world from mutated killer tomatoes.",
+    genre: "Comedy",
+  },
+  {
+    name: "Killer Clowns from Outer Space",
+    description:
+      "Alien clowns invade a small town and begin harvesting humans as a food source.",
+    genre: "Horror",
+  },
+  {
+    name: "Sharknado",
+    description:
+      "A freak hurricane hits Los Angeles, causing man-eating sharks to be scooped up in tornadoes and flood the city with shark-infested seawater.",
+    genre: "Action",
+  },
+  {
+    name: "The Room",
+    description:
+      "A man tries to make sense of his life, love, and friendship after he discovers his best friend and fiancée are having an affair behind his back.",
+    genre: "Drama",
+  },
+  {
+    name: "Space Turtles: The Final Battle",
+    description:
+      "The turtles are back, and this time they're in space! They must battle their way through hordes of evil aliens to save the universe from destruction.",
+    genre: "sci-fi/action",
+  },
+  {
+    name: "The Adventures of Sharkboy and Lavagirl",
+    description:
+      "A young boy dreams up an imaginary world filled with superheroes, including the aquatic Sharkboy and the fiery Lavagirl. But when he is suddenly transported to this world, he must help them fight the evil Mr. Electric and save the day!",
+    genre: "fantasy/adventure",
+  },
+  {
+    name: "The Incredibly Dumb Detective",
+    description:
+      "He's not the sharpest tool in the shed, but he's determined to solve the case. Get ready for a comedy of errors!",
+    genre: "comedy",
+  },
+  {
+    name: "Attack of the Killer Donuts",
+    description:
+      "The donuts have gone rogue and are out for blood. Don't let their sweetness fool you - they're deadly!",
+    genre: "horror",
+  },
+  {
+    name: "The Last Slice of Pizza",
+    description:
+      "Two roommates battle it out for the last slice of pizza in the fridge. Who will emerge victorious in this epic showdown?",
+    genre: "comedy",
+  },
+  {
+    name: "Zombie Cheerleaders",
+    description:
+      "They may be dead, but these cheerleaders still have plenty of pep. Get ready for a undead dance party!",
+    genre: "horror",
+  },
+  {
+    name: "Ninja Cats",
+    description:
+      "They're cute, they're deadly, and they're here to save the day. Watch out for the ninja cats!",
+    genre: "action",
+  },
+  {
+    name: "The Time-Traveling Pigeon",
+    description:
+      "He may look like an ordinary bird, but he's actually a master of time and space. Get ready for a wild ride!",
+    genre: "sci-fi",
+  },
+  {
+    name: "The Haunted Laundromat",
+    description:
+      "Your clothes may come out clean, but at what cost? Beware the ghosts of the laundromat!",
+    genre: "horror",
+  },
+  {
+    name: "The Great Pancake Heist",
+    description:
+      "When the world's supply of maple syrup is at stake, one team of pancake thieves will stop at nothing to get their fill.",
+    genre: "comedy",
+  },
+  {
+    name: "Vampire Accountants",
+    description:
+      "They may be boring by day, but by night these accountants will drain you dry. Watch out for the vampire number-crunchers!",
+    genre: "horror",
+  },
+  {
+    name: "The Secret Life of Llamas",
+    description:
+      "You may think you know everything about these fuzzy animals, but you haven't seen anything yet. Get ready for a wild ride!",
+    genre: "documentary",
+  },
+];
