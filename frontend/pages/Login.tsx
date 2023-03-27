5961b2b715e69101296632f5bb6738ef5e906385
henriquebuzon <henriquebzn1@gmail.com>
2023-03-26 21:49:30 -0300

    Login page part 1
    Restructured login page using Material UI customized components

--- frontend/pages/Login.tsx
+++ frontend/pages/Login.tsx
@@ -1,29 +1,132 @@
-import InputField from "@/components/InputField";
-import { FunctionComponent as FC } from "react";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const Login: FC = () => {
+function Copyright(props: any) {
   return (
-    <>
-      <nav className="px-12 py-5">
-        <img
-          className="w-56 h-56 mx-auto mt-3 mb-8"
-          src="/images/Logo1.png"
-          alt="Logo"
-        />
-      </nav>
-      <div className="flex justify-center">
-        <div className="bg-neutral-200 px-8 py-8 self-center lg:w-2/5 lg:max-w-md rounded-md w-full ">
-          <h2 className="text-neutral-800 text-3xl mb-font-semibold mb-5 font-bold ">
-            Login
-          </h2>
-          <div className="flex flex-col gap-4">
-            <InputField />
-          </div>
-        </div>
-      </div>
-    </>
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Nextflix
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
   );
-};
+}
 
-export default Login;
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default function SignIn() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <img
+            className="w-64 h-64 mx-auto mt-1 mb-20"
+            src="/images/Logo1.png"
+            alt="Logo"
+          />
+          <Typography
+            component="h1"
+            variant="h5"
+            align="left"
+            marginTop={4}
+            marginBottom={1}
+          >
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate>
+            <TextField
+              margin="dense"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              size="small"
+            />
+            <TextField
+              margin="dense"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              size="small"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              style={{ backgroundColor: "red" }}
+              sx={{
+                mt: 3,
+                mb: 2,
+                color: "#ffffff",
+              }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
