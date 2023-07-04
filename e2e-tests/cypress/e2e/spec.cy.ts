0767dfc67ab039bdee1c313ddfa3211b6fbd6ddc
Henrique <henriquebzn1@gmail.com>
2023-07-03 21:53:53 -0300

    Intercepted api requests with Cypress

--- e2e-tests/cypress/e2e/spec.cy.ts
+++ e2e-tests/cypress/e2e/spec.cy.ts
@@ -5,11 +5,14 @@ describe('Core functionality', () => {
   })
 
   it('watches a movie', () => {
+    cy.intercept('api/auth').as('authRequest')
+    cy.intercept('api/movies*').as('moviesRequest')
     cy.get('input[name="username"]').type('testuser')
     cy.get('input[name="password"]').type('testpassword')
     cy.get('[data-cy="sigin-button"]').click().should('be.disabled')
-    cy.wait(10000)
+    cy.wait('@authRequest')
     cy.location('pathname').should('equal', '/')
+    cy.wait('@moviesRequest')
     cy.get('[data-cy="movie-card"]').first().click()
     cy.get('[data-cy="watch-button"]').click()
     cy.get('video').should('exist')
